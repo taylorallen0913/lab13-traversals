562af88c1acfbc215d8ef92aa7ef87bc7e680610
ChronosYan <christiantghnc@gmail.com>
2022-05-06 12:50:10 -0700

    Added recursive and stack implementation of InOrder

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -3,32 +3,32 @@ package bst;
 import java.util.Stack;
 
 public class BinarySearchTree<T extends Comparable<T>> {
-	
+
 	private static class BSTNode<T extends Comparable<T>>{
 		private T data;
 		private BSTNode<T> leftChild;
 		private BSTNode<T> rightChild;
-		
+
 		public BSTNode(T data) {
 			this.data = data;
 		}
-		
+
 		public String toString() {
 			return data.toString();
 		}
 	}
-	
+
 	private BSTNode<T> root;
-	
+
 	public void insert(T data) {
 		root = recursiveInsert(root,data);
 	}
-	
+
 	private BSTNode<T> recursiveInsert(BSTNode<T> node, T data) {
 		if(node == null) {
 			return new BSTNode<T>(data);
 		}
-		
+
 		else if(data.compareTo(node.data)<0) {
 			node.leftChild = recursiveInsert(node.leftChild,data);
 		}
@@ -37,44 +37,44 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 		return node;
 	}
-	
+
 	public void delete(T data) {
 		root = recursiveDelete(root,data);
 	}
-	
+
 	private BSTNode<T> recursiveDelete(BSTNode<T> node,T data){
 		if(node == null) {
 			return node;
 		}
 		else {
-		if(data.compareTo(node.data)<0) {
-			node.leftChild = recursiveDelete(node.leftChild,data);
-		}
-		else if(data.compareTo(node.data)>0) {
-			node.rightChild = recursiveDelete(node.rightChild,data);
-		}
-		else {//we found the node to delete
-			if(node.leftChild==null && node.rightChild == null) {
-				return null;
+			if(data.compareTo(node.data)<0) {
+				node.leftChild = recursiveDelete(node.leftChild,data);
 			}
-			else if(node.leftChild == null) {
-				return node.rightChild;
+			else if(data.compareTo(node.data)>0) {
+				node.rightChild = recursiveDelete(node.rightChild,data);
 			}
-			else if(node.rightChild == null) {
-				return node.leftChild;
+			else {//we found the node to delete
+				if(node.leftChild==null && node.rightChild == null) {
+					return null;
+				}
+				else if(node.leftChild == null) {
+					return node.rightChild;
+				}
+				else if(node.rightChild == null) {
+					return node.leftChild;
+				}
+				else {//Still need to handle the case with two children
+					BSTNode<T> predecessor = getMax(node.leftChild);
+					T d = predecessor.data;
+					node.data = d;//update data at node
+					//remove predecessor node
+					node.leftChild = recursiveDelete(node.leftChild,d);
+				}
 			}
-			else {//Still need to handle the case with two children
-				BSTNode<T> predecessor = getMax(node.leftChild);
-				T d = predecessor.data;
-				node.data = d;//update data at node
-				//remove predecessor node
-				node.leftChild = recursiveDelete(node.leftChild,d);
-			}
-		}
-		return node;
+			return node;
 		}
 	}
-	
+
 	//assumes root is not null
 	public BSTNode<T> getMax(BSTNode<T> node){
 		while(node.rightChild!= null) {
@@ -90,15 +90,15 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 		return node;
 	}
-	
+
 	public boolean contains(T data) {
 		return find(data)!=null;
 	}
-	
+
 	public BSTNode<T> find(T key) {
 		return recursiveFind(root,key);
 	}
-	
+
 	private BSTNode<T> recursiveFind(BSTNode<T> node,T key) {
 		//base case, made it to the end or I found it
 		if(node == null || key.equals(node.data)) {
@@ -110,31 +110,31 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		else {
 			return recursiveFind(node.rightChild,key);
 		}
-		
+
 	}
-	
+
 	//Traverse the tree in an preorder fashion
 	//Print the current node first and then recurse on the children
 	public void preOrder() {
 		System.out.println("PreOrder test commit");
 		preOrderRecurse(root); 
 	}
-	
+
 	private void preOrderRecurse(BSTNode<T> node) {
 		if(node == null) return;
 		System.out.println(node.data);
 		preOrderRecurse(node.leftChild);
 		preOrderRecurse(node.rightChild);
 	}
-	
+
 	//Traverse the tree in an preorder fashion but using a stack
 	//Print the current node first and then recurse on the children
 	public void preOrderStack() {
 		Stack<BSTNode<T>> pre = new Stack<BSTNode<T>>();
-		
-		
+
+
 	}
-		
+
 
 	//Traverse the tree in an inorder fashion
 	//Recursively print the left side of the current node, then the current node, 
@@ -143,35 +143,49 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	public void inOrder() {
 		inOrderRecurse(root); 
 	}
-	
+
 	public void inOrderRecurse(BSTNode<T> node) {
 		if(node == null) {
 			return;
 		}
-		
+
 		inOrderRecurse(node.leftChild);
 		System.out.println(node.data);
 		inOrderRecurse(node.rightChild);
+		
 	}
-	
+
 	//Traverse the tree in an inorder fashion but using a stack
 	public void inOrderStack() {
 		Stack<BSTNode<T>> in = new Stack<BSTNode<T>>();
-		in.add(root);
-		
-		while(in != n)
+
+		if(root == null) {
+			return;
+		}
+
+		BSTNode<T> node = root;
+
+		while(node != null || in.size() > 0) {
+			while(node != null) {
+				in.push(node);
+				node = node.leftChild;
+			}
+			node = in.pop();
+			System.out.println(node.data);	
+			node = node.rightChild;
+		}
 	}
-	
+
 	//Traverse the tree in an postorder fashion
 	//Recurse on the children and then print the value in the current node
 	public void postOrder() {
 		postOrderRecurse(root); 
 	}
-	
+
 	public void postOrderRecurse(BSTNode<T> node) {
-		
+
 	}
-	
+
 	//Traverse the tree in an postorder fashion uses Stacks. 
 	//This is more difficult than the other traversals using a Stack
 	//I suggest using two stacks. Think about the order you want the elements
@@ -184,7 +198,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 			while(!postHelper.isEmpty()) {
 				//how should post and postHelper be updated?
 			}
-			
+
 			while(!post.isEmpty()) {
 				BSTNode<T> node = post.pop();
 				System.out.print(node + " ");
@@ -192,21 +206,21 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		}
 
 	}
-	
+
 	public String toString() {
 		return recursiveToString(root, "");		
 	}	
 
 	private String recursiveToString(BSTNode<T> node, String indent) {
-		
+
 		if(node == null) {return "";}
 		else {
 			return recursiveToString(node.rightChild,indent + "    ")+ 
-			"\n" + indent  +node.data +
-			recursiveToString(node.leftChild,indent + "    ");
+					"\n" + indent  +node.data +
+					recursiveToString(node.leftChild,indent + "    ");
 		}	
 	}
-	
+
 	public static void main(String[] args) {
 		//Test Tree
 		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
@@ -219,7 +233,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		bst.insert(10);
 		bst.insert(3);
 		System.out.println(bst);
-	
+
 		System.out.println("In Order Traversals");
 		bst.inOrder();
 		System.out.println();
@@ -234,10 +248,10 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		bst.postOrder();
 		System.out.println();
 		bst.postOrderStack();
-		
-		
+
+
 	}
-	
+
 
 }
 
