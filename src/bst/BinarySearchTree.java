8389ce33cd9c02f91f98f6897f986335d31c63f4
gr33npo1s0n <iyerchandramouli03@gmail.com>
2022-05-06 11:21:46 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -3,203 +3,201 @@ package bst;
 import java.util.Stack;
 
 public class BinarySearchTree<T extends Comparable<T>> {
-	
-	private static class BSTNode<T extends Comparable<T>>{
+
+	private static class BSTNode<T extends Comparable<T>> {
 		private T data;
 		private BSTNode<T> leftChild;
 		private BSTNode<T> rightChild;
-		
+
 		public BSTNode(T data) {
 			this.data = data;
 		}
-		
+
+		@Override
 		public String toString() {
-			return data.toString();
+			return this.data.toString();
 		}
 	}
-	
+
 	private BSTNode<T> root;
-	
+
 	public void insert(T data) {
-		root = recursiveInsert(root,data);
+		this.root = this.recursiveInsert(this.root, data);
 	}
-	
+
 	private BSTNode<T> recursiveInsert(BSTNode<T> node, T data) {
-		if(node == null) {
+		if (node == null) {
 			return new BSTNode<T>(data);
 		}
-		
-		else if(data.compareTo(node.data)<0) {
-			node.leftChild = recursiveInsert(node.leftChild,data);
-		}
-		else if(data.compareTo(node.data)>0) {
-			node.rightChild = recursiveInsert(node.rightChild,data);
+		if (data.compareTo(node.data) < 0) {
+			node.leftChild = this.recursiveInsert(node.leftChild, data);
+		} else if (data.compareTo(node.data) > 0) {
+			node.rightChild = this.recursiveInsert(node.rightChild, data);
 		}
 		return node;
 	}
-	
+
 	public void delete(T data) {
-		root = recursiveDelete(root,data);
+		this.root = this.recursiveDelete(this.root, data);
 	}
-	
-	private BSTNode<T> recursiveDelete(BSTNode<T> node,T data){
-		if(node == null) {
+
+	private BSTNode<T> recursiveDelete(BSTNode<T> node, T data) {
+		if (node == null) {
 			return node;
 		}
-		else {
-		if(data.compareTo(node.data)<0) {
-			node.leftChild = recursiveDelete(node.leftChild,data);
-		}
-		else if(data.compareTo(node.data)>0) {
-			node.rightChild = recursiveDelete(node.rightChild,data);
-		}
-		else {//we found the node to delete
-			if(node.leftChild==null && node.rightChild == null) {
+		if (data.compareTo(node.data) < 0) {
+			node.leftChild = this.recursiveDelete(node.leftChild, data);
+		} else if (data.compareTo(node.data) > 0) {
+			node.rightChild = this.recursiveDelete(node.rightChild, data);
+		} else {// we found the node to delete
+			if (node.leftChild == null && node.rightChild == null) {
 				return null;
-			}
-			else if(node.leftChild == null) {
+			} else if (node.leftChild == null) {
 				return node.rightChild;
-			}
-			else if(node.rightChild == null) {
+			} else if (node.rightChild == null) {
 				return node.leftChild;
-			}
-			else {//Still need to handle the case with two children
-				BSTNode<T> predecessor = getMax(node.leftChild);
+			} else {// Still need to handle the case with two children
+				BSTNode<T> predecessor = this.getMax(node.leftChild);
 				T d = predecessor.data;
-				node.data = d;//update data at node
-				//remove predecessor node
-				node.leftChild = recursiveDelete(node.leftChild,d);
+				node.data = d;// update data at node
+				// remove predecessor node
+				node.leftChild = this.recursiveDelete(node.leftChild, d);
 			}
 		}
 		return node;
-		}
 	}
-	
-	//assumes root is not null
-	public BSTNode<T> getMax(BSTNode<T> node){
-		while(node.rightChild!= null) {
+
+	// assumes root is not null
+	public BSTNode<T> getMax(BSTNode<T> node) {
+		while (node.rightChild != null) {
 			node = node.rightChild;
 		}
 		return node;
 	}
 
-	//assumes root is not null
-	public BSTNode<T> getMin(BSTNode<T> node){
-		while(node.leftChild!=null) {
+	// assumes root is not null
+	public BSTNode<T> getMin(BSTNode<T> node) {
+		while (node.leftChild != null) {
 			node = node.leftChild;
 		}
 		return node;
 	}
-	
+
 	public boolean contains(T data) {
-		return find(data)!=null;
+		return this.find(data) != null;
 	}
-	
+
 	public BSTNode<T> find(T key) {
-		return recursiveFind(root,key);
+		return this.recursiveFind(this.root, key);
 	}
-	
-	private BSTNode<T> recursiveFind(BSTNode<T> node,T key) {
-		//base case, made it to the end or I found it
-		if(node == null || key.equals(node.data)) {
+
+	private BSTNode<T> recursiveFind(BSTNode<T> node, T key) {
+		// base case, made it to the end or I found it
+		if (node == null || key.equals(node.data)) {
 			return node;
 		}
-		if(key.compareTo(node.data)<0) {
-			return recursiveFind(node.leftChild,key);
+		if (key.compareTo(node.data) < 0) {
+			return this.recursiveFind(node.leftChild, key);
 		}
-		else {
-			return recursiveFind(node.rightChild,key);
-		}
-		
+		return this.recursiveFind(node.rightChild, key);
+
 	}
-	
-	//Traverse the tree in an preorder fashion
-	//Print the current node first and then recurse on the children
+
+	// Traverse the tree in an preorder fashion
+	// Print the current node first and then recurse on the children
 	public void preOrder() {
+<<<<<<< HEAD
 		System.out.println("PreOrder test commit");
 		preOrderRecurse(root); 
+=======
+		this.preOrderRecurse(this.root);
+>>>>>>> 5cdaac2 (updated traversal code)
 	}
-	
+
 	private void preOrderRecurse(BSTNode<T> node) {
+<<<<<<< HEAD
 		if(node == null) return;
 		System.out.println(node.data);
 		preOrderRecurse(node.leftChild);
 		preOrderRecurse(node.rightChild);
+=======
+
+>>>>>>> 5cdaac2 (updated traversal code)
 	}
-	
-	//Traverse the tree in an preorder fashion but using a stack
-	//Print the current node first and then recurse on the children
+
+	// Traverse the tree in an preorder fashion but using a stack
+	// Print the current node first and then recurse on the children
 	public void preOrderStack() {
 		Stack<BSTNode<T>> pre = new Stack<BSTNode<T>>();
-		
-	}
-		
 
-	//Traverse the tree in an inorder fashion
-	//Recursively print the left side of the current node, then the current node, 
-	//then recursively print the right side of current node
-	//For a bst this will print the values in sorted order from smallest to largest
+	}
+
+	// Traverse the tree in an inorder fashion
+	// Recursively print the left side of the current node, then the current node,
+	// then recursively print the right side of current node
+	// For a bst this will print the values in sorted order from smallest to largest
 	public void inOrder() {
-		inOrderRecurse(root); 
+		this.inOrderRecurse(this.root);
 	}
-	
+
 	public void inOrderRecurse(BSTNode<T> node) {
-		
+
 	}
-	//Traverse the tree in an inorder fashion but using a stack
+
+	// Traverse the tree in an inorder fashion but using a stack
 	public void inOrderStack() {
 		Stack<BSTNode<T>> in = new Stack<BSTNode<T>>();
-		
-		
+
 	}
-	
-	//Traverse the tree in an postorder fashion
-	//Recurse on the children and then print the value in the current node
+
+	// Traverse the tree in an postorder fashion
+	// Recurse on the children and then print the value in the current node
 	public void postOrder() {
-		postOrderRecurse(root); 
+		this.postOrderRecurse(this.root);
+		System.out.println("PostOrder test commit");
 	}
-	
+
 	public void postOrderRecurse(BSTNode<T> node) {
-		
+
 	}
-	
-	//Traverse the tree in an postorder fashion uses Stacks. 
-	//This is more difficult than the other traversals using a Stack
-	//I suggest using two stacks. Think about the order you want the elements
-	//to appear on the stack you will print.
+
+	// Traverse the tree in an postorder fashion uses Stacks.
+	// This is more difficult than the other traversals using a Stack
+	// I suggest using two stacks. Think about the order you want the elements
+	// to appear on the stack you will print.
 	public void postOrderStack() {
 		Stack<BSTNode<T>> post = new Stack<>();
 		Stack<BSTNode<T>> postHelper = new Stack<>();
-		if(root!=null) {
-			postHelper.push(root);
-			while(!postHelper.isEmpty()) {
-				//how should post and postHelper be updated?
+		if (this.root != null) {
+			postHelper.push(this.root);
+			while (!postHelper.isEmpty()) {
+				// how should post and postHelper be updated?
 			}
-			
-			while(!post.isEmpty()) {
+
+			while (!post.isEmpty()) {
 				BSTNode<T> node = post.pop();
 				System.out.print(node + " ");
 			}
 		}
 
 	}
-	
+
+	@Override
 	public String toString() {
-		return recursiveToString(root, "");		
-	}	
+		return this.recursiveToString(this.root, "");
+	}
 
 	private String recursiveToString(BSTNode<T> node, String indent) {
-		
-		if(node == null) {return "";}
-		else {
-			return recursiveToString(node.rightChild,indent + "    ")+ 
-			"\n" + indent  +node.data +
-			recursiveToString(node.leftChild,indent + "    ");
-		}	
+
+		if (node == null) {
+			return "";
+		}
+		return this.recursiveToString(node.rightChild, indent + "    ") + "\n" + indent + node.data
+				+ this.recursiveToString(node.leftChild, indent + "    ");
 	}
-	
+
 	public static void main(String[] args) {
-		//Test Tree
+		// Test Tree
 		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
 		bst.insert(9);
 		bst.insert(7);
@@ -210,7 +208,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		bst.insert(10);
 		bst.insert(3);
 		System.out.println(bst);
-	
+
 		System.out.println("In Order Traversals");
 		bst.inOrder();
 		System.out.println();
@@ -225,10 +223,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		bst.postOrder();
 		System.out.println();
 		bst.postOrderStack();
-		
-		
+
 	}
-	
 
 }
-
